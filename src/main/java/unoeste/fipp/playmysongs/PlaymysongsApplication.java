a2b1858e9b203621671005533486b1decac7cdff
RafaaelSF09 <rafaelfiori8@gmail.com>
2026-03-10 11:16:18 -0300

    primeiro commit

---
+++ src/main/java/unoeste/fipp/playmysongs/PlaymysongsApplication.java
@@ -0,0 +1,13 @@
+package unoeste.fipp.playmysongs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaymysongsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaymysongsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unoeste/fipp/playmysongs/PlaymysongsApplicationTests.java
@@ -0,0 +1,13 @@
+package unoeste.fipp.playmysongs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaymysongsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
